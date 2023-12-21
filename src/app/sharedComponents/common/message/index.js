7469a158726897fd032dfd64ca49550695b0cfaf
MohamedMagdi0 <mohamed.magdi.deif@gmail.com>
2023-12-21 12:02:17 +0200

    feat: adds error/warning messages reusable Components

---
+++ src/app/sharedComponents/common/message/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
+
+import styles from './styles';
+import colors from '../../../../../public/themes/colors';
+
+const Message = ({
+  message,
+  onDismiss,
+  retry,
+  retryFn,
+  primary,
+  danger,
+  info,
+  success,
+}) => {
+  const [userDismissed, setDismissed] = React.useState(false);
+
+  const getBgColor = () => {
+    if (primary) {
+      return colors.primary;
+    }
+    if (danger) {
+      return colors.danger;
+    }
+    if (success) {
+      return colors.success;
+    }
+
+    if (info) {
+      return colors.secondary;
+    }
+  };
+  return (
+    <>
+      {userDismissed ? null : (
+        <TouchableOpacity
+          style={[styles.wrapper, {backgroundColor: getBgColor()}]}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{
+                color: colors.white,
+              }}>
+              {message}
+            </Text>
+
+            {retry && typeof onDismiss !== 'function' && (
+              <TouchableOpacity onPress={retryFn}>
+                <Text
+                  style={{
+                    color: colors.white,
+                  }}>
+                  Retry
+                </Text>
+              </TouchableOpacity>
+            )}
+
+            {typeof onDismiss === 'function' && (
+              <TouchableOpacity
+                onPress={() => {
+                  setDismissed(true);
+                  onDismiss();
+                }}>
+                <Text
+                  style={{
+                    color: colors.white,
+                  }}>
+                  X
+                </Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        </TouchableOpacity>
+      )}
+    </>
+  );
+};
+
+export default Message;
