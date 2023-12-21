dd45412f79333a8ee4025b6e597524a7130dbb65
MohamedMagdi0 <mohamed.magdi.deif@gmail.com>
2023-12-21 11:56:27 +0200

    feat: adds app color theme

---
+++ public/themes/colors.js
@@ -0,0 +1,11 @@
+export default {
+  primary: '#1DA1F2',
+  secondary: '#3f37c9',
+  accent: '#4895ef',
+  danger: '#f72585',
+  success: '#4cc9f0',
+  grey: '#adb5bd',
+  white: '#ffffff',
+  AvatarBackgroundColor: '#d9d9d9',
+  phoneNumber: '#888',
+};
