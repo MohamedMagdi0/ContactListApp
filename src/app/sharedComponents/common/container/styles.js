54d56f7398ea54abe5bd163b334295161929f7c6
MohamedMagdi0 <mohamed.magdi.deif@gmail.com>
2023-12-21 11:58:34 +0200

    feat : adds Container reusable Components

---
+++ src/app/sharedComponents/common/container/styles.js
@@ -0,0 +1,10 @@
+import {Dimensions, StyleSheet} from 'react-native';
+import colors from '../../../../../public/themes/colors';
+
+export default StyleSheet.create({
+  wrapper: {
+    padding: 20,
+    // backgroundColor: colors.grey,
+    // height: Dimensions.get('window').height * 1.05,
+  },
+});
