6b6d1aa27e80c547a84a251daaae2daf18aa65dd
MohamedMagdi0 <mohamed.magdi.deif@gmail.com>
2023-12-21 12:09:06 +0200

    feat: adds single contact reusable Components

---
+++ src/app/modules/ContactList/components/SingleContact/style.js
@@ -0,0 +1,42 @@
+import {StyleSheet} from 'react-native';
+
+export default styles = StyleSheet.create({
+  contactCon: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 5,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#d9d9d9',
+  },
+  imgCon: {},
+  placeholder: {
+    width: 55,
+    height: 55,
+    borderRadius: 30,
+    overflow: 'hidden',
+    backgroundColor: '#d9d9d9',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contactDat: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 5,
+  },
+  txt: {
+    fontSize: 18,
+  },
+  name: {
+    fontSize: 16,
+    textAlign: 'left',
+  },
+  phoneNumber: {
+    color: '#888',
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    alignSelf: 'center',
+    borderRadius: 25,
+  },
+});
